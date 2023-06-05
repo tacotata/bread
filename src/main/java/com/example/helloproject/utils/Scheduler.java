db31629f75f52410152c0f5a8b50dfdb384c40a5
tacotata <71251163+tacotata@users.noreply.github.com>
2023-06-05 20:59:12 +0900

    주석 삭제

--- src/main/java/com/example/helloproject/utils/Scheduler.java
+++ src/main/java/com/example/helloproject/utils/Scheduler.java
@@ -26,31 +26,31 @@ public class Scheduler {
 
     @Scheduled(fixedDelay = 1000)
     public void scheduleFixedDelayTask() throws InterruptedException {
-//        log.info("===========SCHEDULER START=================");
-//        log.info("Fixed delay task - {}", System.currentTimeMillis() / 1000);
-//        LocalDateTime now = LocalDateTime.now();
-//        List<CartResponseDto> cart = cartRepository.findAll().stream().map(CartResponseDto::new).collect(Collectors.toList());
-//        try {
-//          if(!cart.isEmpty()) {
-//            for (int i = 0; i < cart.size(); i++) {
-//              log.info("REGDATE {}", String.valueOf(cart.get(i).getRegDate()));
-//              log.info("CART ID {}", String.valueOf(cart.get(i).getId()));
-//              if (ChronoUnit.MINUTES.between(cart.get(i).getRegDate(), now) >= 30) {
-//                List<Long> cartItemList = cartService.deleteCartItems(cart.get(i).getId());
-//                if (!cartItemList.isEmpty()) {
-//                  for (Long cartItem : cartItemList) {
-//                    log.info("DELETE CART ITEM ID : {}", cartItem);
-//                  }
-//                }
-//                cartService.deleteCart(cart.get(i).getId());
-//                log.info("DELETE CART ID : {}", cart.get(i).getId());
-//              }
-//            }
-//          }
-//
-//        }catch (Exception e){
-//          log.info(e.getMessage());
-//        }
-//        log.info("===========SCHEDULER END=================");
+        log.info("===========SCHEDULER START=================");
+        log.info("Fixed delay task - {}", System.currentTimeMillis() / 1000);
+        LocalDateTime now = LocalDateTime.now();
+        List<CartResponseDto> cart = cartRepository.findAll().stream().map(CartResponseDto::new).collect(Collectors.toList());
+        try {
+         if(!cart.isEmpty()) {
+            for (int i = 0; i < cart.size(); i++) {
+              log.info("REGDATE {}", String.valueOf(cart.get(i).getRegDate()));
+              log.info("CART ID {}", String.valueOf(cart.get(i).getId()));
+              if (ChronoUnit.MINUTES.between(cart.get(i).getRegDate(), now) >= 30) {
+                List<Long> cartItemList = cartService.deleteCartItems(cart.get(i).getId());
+                if (!cartItemList.isEmpty()) {
+                  for (Long cartItem : cartItemList) {
+                    log.info("DELETE CART ITEM ID : {}", cartItem);
+                  }
+                }
+                cartService.deleteCart(cart.get(i).getId());
+                log.info("DELETE CART ID : {}", cart.get(i).getId());
+              }
+            }
+          }
+
+        }catch (Exception e){
+          log.info(e.getMessage());
+        }
+        log.info("===========SCHEDULER END=================");
     }
 }
